f9a6baf118af2b39b58870b6c54ec8522ad48f31
AMAN KAUSHIK <84031878+amankaushik3919@users.noreply.github.com>
2021-05-14 20:22:44 +0530

    Add files via upload

---
+++ tutorial_0.py
@@ -0,0 +1,23 @@
+a = 12
+b = 12
+c = 14
+d = 15
+z = 90
+water_bottle = "water"
+_ = 'pop'
+print(type(_)) #I USED 2 FUNCTIONS 'PRINT' AND 'TYPE'
+bottle = 910
+"""
+[]{}()*/-+!~`:;<>/,./\
+"""
+"""
+a have the value 12
+b is 12
+c is 14
+d is 15
+with z is 90
+this _ <-- dash can br used for taking the last result in our python shell
+"""
+pop = 45+5
+print(pop)
+print(_+8)
